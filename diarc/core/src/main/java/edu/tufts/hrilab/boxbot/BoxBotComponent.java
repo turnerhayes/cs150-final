6d652deb342a32e833166b46910b7da2c93a9c1c
Turner Hayes <dev@turnerhayes.com>
2024-11-16 13:55:42 -0500

    Add interface and component for BoxBot simulator

---
+++ diarc/core/src/main/java/edu/tufts/hrilab/boxbot/BoxBotComponent.java
@@ -0,0 +1,33 @@
+package edu.tufts.hrilab.boxbot;
+
+import edu.tufts.hrilab.action.justification.Justification;
+import edu.tufts.hrilab.diarc.DiarcComponent;
+import edu.tufts.hrilab.interfaces.BoxBotSimulatorInterface;
+
+public class BoxBotComponent extends DiarcComponent implements BoxBotSimulatorInterface {
+
+    @Override
+    public Justification moveLeft() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'moveLeft'");
+    }
+
+    @Override
+    public Justification moveRight() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'moveRight'");
+    }
+
+    @Override
+    public Justification moveUp() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'moveUp'");
+    }
+
+    @Override
+    public Justification moveDown() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'moveDown'");
+    }
+    
+}
