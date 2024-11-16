7277c63c75f2c60a97dd7b22437beb2503c829e8
Turner Hayes <dev@turnerhayes.com>
2024-11-16 17:07:16 -0500

    Add speech component interface to issue commands
    - Note that this does not currently work, in that clicking one of the commands will error out. This is just a starting point.

--- diarc/config/src/main/java/edu/tufts/hrilab/config/boxbot/BoxBotConfig.java
+++ diarc/config/src/main/java/edu/tufts/hrilab/config/boxbot/BoxBotConfig.java
@@ -1,12 +1,26 @@
 package edu.tufts.hrilab.config.boxbot;
 
 import edu.tufts.hrilab.diarc.DiarcConfiguration;
+import edu.tufts.hrilab.simspeech.SimSpeechRecognitionComponent;
+import edu.tufts.hrilab.slug.listen.ListenerComponent;
+import edu.tufts.hrilab.action.GoalManagerComponent;
+import edu.tufts.hrilab.boxbot.BoxBotComponent;
 
 public class BoxBotConfig extends DiarcConfiguration {
     
   // start the configuration
   @Override
   public void runConfiguration() {
+    createInstance(SimSpeechRecognitionComponent.class,
+              "-config boxbot.simspeech -speaker turner -addressee boxbot");
+    createInstance(ListenerComponent.class);
+    
+    createInstance(BoxBotComponent.class, "-service parseIt");
 
+    String gmArgs = "-beliefinitfile demos.pl agents/twonaoagents.pl " +
+            "-asl core.asl vision.asl nao/naodemo.asl dialogue/nlg.asl dialogue/handleSemantics.asl dialogue/nlu.asl " +
+            "-goal listen(self)";
+
+    createInstance(GoalManagerComponent.class, gmArgs);
   }
 }
--- diarc/core/src/main/java/edu/tufts/hrilab/boxbot/BoxBotComponent.java
+++ diarc/core/src/main/java/edu/tufts/hrilab/boxbot/BoxBotComponent.java
@@ -1,10 +1,136 @@
 package edu.tufts.hrilab.boxbot;
 
+import ai.thinkingrobots.trade.TRADE;
+import ai.thinkingrobots.trade.TRADEException;
+import ai.thinkingrobots.trade.TRADEService;
+import ai.thinkingrobots.trade.TRADEServiceConstraints;
+import edu.tufts.hrilab.fol.Symbol;
+import edu.tufts.hrilab.fol.Variable;
+import edu.tufts.hrilab.fol.Predicate;
+import edu.tufts.hrilab.fol.Factory;
 import edu.tufts.hrilab.action.justification.Justification;
 import edu.tufts.hrilab.diarc.DiarcComponent;
 import edu.tufts.hrilab.interfaces.BoxBotSimulatorInterface;
+import edu.tufts.hrilab.interfaces.NLUInterface;
+import edu.tufts.hrilab.slug.common.UtteranceType;
+import edu.tufts.hrilab.slug.parsing.llm.AlternateResponse;
+import edu.tufts.hrilab.slug.parsing.llm.Descriptor;
+import edu.tufts.hrilab.slug.parsing.llm.Intention;
+import edu.tufts.hrilab.slug.parsing.llm.ParserResponse;
+import edu.tufts.hrilab.slug.parsing.llm.Proposition;
+import edu.tufts.hrilab.slug.parsing.llm.Referent;
+import edu.tufts.hrilab.slug.common.Utterance;
 
-public class BoxBotComponent extends DiarcComponent implements BoxBotSimulatorInterface {
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.commons.cli.CommandLine;
+import org.apache.commons.cli.Option;
+
+
+public class BoxBotComponent extends DiarcComponent implements BoxBotSimulatorInterface, NLUInterface {
+    private String service = null;
+
+   @Override
+   protected List<Option> additionalUsageInfo() {
+     List<Option> options = new ArrayList<>();
+     options.add(Option.builder("service").hasArg().argName("string").desc("TRADE Service to call in place of parseUtterance").build());
+     return options;
+   }
+
+    @Override
+    protected void parseArgs(CommandLine cmdLine) {
+        if (cmdLine.hasOption("service")) {
+            service = cmdLine.getOptionValue("service");
+        }
+    }
+
+    /**
+     * Parses the input Utterance to obtain semantic information.
+     * Optionally use a TRADEService defined in the service property.
+     * Default behavior is to use getResponse() to retrieve from parser service.
+     *
+     * @param input The input Utterance to parse.
+     * @return The parsed Utterance with updated semantic information.
+     */
+    @Override
+    public Utterance parseUtterance(Utterance input) {
+        String inputString = input.getWordsAsString();
+        AlternateResponse altResponse;
+        ParserResponse response = null;
+        Symbol addressee = null;
+
+        log.info("parseUtterance: " + inputString);
+
+        try {
+            altResponse = TRADE.getAvailableService(new TRADEServiceConstraints().name(service).argTypes(String.class))
+                    .call(AlternateResponse.class, inputString);
+            response = altResponse.response;
+            if (altResponse.addressee != null) {
+                addressee = altResponse.addressee;
+                log.debug("Got addressee from alternate LLM service: " + addressee.toString());
+            }
+        } catch (TRADEException ex) {
+            log.error("Error calling " + service + ".", ex);
+        }
+
+        if (response == null) {
+            log.error("Error: Failed to get response");
+            return null;
+        } else {
+            log.info("Got response: " + response.toString());
+        }
+        Utterance.Builder output = new Utterance.Builder(input);
+        output.setUtteranceType(UtteranceType.valueOf(response.intention.intent.toUpperCase()));
+
+        // collect referent info by variable name
+        Map<String, Referent> referentMap = new HashMap<>();
+        List<Variable> variables = new ArrayList<>();
+        for (Referent referent : response.referents) {
+            referentMap.put(referent.variable_name, referent);
+            variables.add(Factory.createVariable(referent.variable_name, referent.type));
+        }
+
+        // populate semantics with semantically typed variables
+        Proposition prop = response.intention.proposition;
+        Predicate semantics;
+        if (UtteranceType.valueOf(response.intention.intent.toUpperCase()) == UtteranceType.INSTRUCT) {
+            List<Symbol> args = new ArrayList<>();
+            // EW MultiLingualPickAndPlaceDemo: Can't do this because of possibility of
+            // direct address. Response is supplying the actor
+            if (!service.equals("pickAndPlaceLLMParser")) {
+                args.add(input.getAddressee());
+            } else if (addressee != null) {
+                args.add(addressee);
+                output.setAddressee(addressee);
+            } else {
+                args.add(input.getAddressee());
+                output.setAddressee(input.getAddressee());
+            }
+            Arrays.stream(prop.arguments).forEach(arg -> args.add(Factory.createFOL(arg)));
+            semantics = Factory.createPredicate(prop.text, args);
+        } else {
+            semantics = Factory.createPredicate(prop.text, prop.arguments);
+        }
+        output.setSemantics(semantics.copyWithNewVariableTypes(variables));
+
+        // populate supplemental semantics
+        for (Descriptor descriptor : response.descriptors) {
+            Predicate descriptorPred = Factory.createPredicate(descriptor.text, descriptor.arguments);
+            output.addSupplementalSemantics(descriptorPred.copyWithNewVariableTypes(variables));
+        }
+
+        // add tier assignments to supplemental semantics
+        variables.forEach(var -> output.addTierAssignment(var,
+                Factory.createSymbol(referentMap.get(var.getName()).toString().toUpperCase())));
+        // variables.forEach(var -> output.addTierAssignment(var,
+        // Factory.createSymbol(referentMap.get(var.getName()).cognitive_status.toUpperCase())));
+
+        return output.build();
+    }
 
     @Override
     public Justification moveLeft() {
@@ -29,5 +155,14 @@ public class BoxBotComponent extends DiarcComponent implements BoxBotSimulatorIn
         // TODO Auto-generated method stub
         throw new UnsupportedOperationException("Unimplemented method 'moveDown'");
     }
-    
+
+    @TRADEService
+    public AlternateResponse parseIt(String input) {
+
+        ParserResponse pr = new ParserResponse();
+        pr.intention = new Intention();
+        pr.intention.intent = input;
+
+        return new AlternateResponse(pr, new Symbol("roboshopper"));
+    }
 }
