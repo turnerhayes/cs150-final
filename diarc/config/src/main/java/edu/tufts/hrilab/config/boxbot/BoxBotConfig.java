14aa5c4315dd4d64bfacbdbd3ca408f01630c56f
Turner Hayes <dev@turnerhayes.com>
2024-11-23 11:57:27 -0500

    Remove NLU components from BoxBot
    - We can use the web UI to trigger actions instead

--- diarc/config/src/main/java/edu/tufts/hrilab/config/boxbot/BoxBotConfig.java
+++ diarc/config/src/main/java/edu/tufts/hrilab/config/boxbot/BoxBotConfig.java
@@ -1,8 +1,6 @@
 package edu.tufts.hrilab.config.boxbot;
 
 import edu.tufts.hrilab.diarc.DiarcConfiguration;
-import edu.tufts.hrilab.simspeech.SimSpeechRecognitionComponent;
-import edu.tufts.hrilab.slug.listen.ListenerComponent;
 import edu.tufts.hrilab.action.GoalManagerComponent;
 import edu.tufts.hrilab.boxbot.BoxBotComponent;
 
@@ -11,19 +9,11 @@ public class BoxBotConfig extends DiarcConfiguration {
   // start the configuration
   @Override
   public void runConfiguration() {
-    createInstance(SimSpeechRecognitionComponent.class,
-              "-config boxbot.simspeech -speaker admin -addressee boxbot");
-    createInstance(ListenerComponent.class);
-    
-    createInstance(BoxBotComponent.class, "-service parseIt");
+    createInstance(BoxBotComponent.class);
 
     String[] aslFiles = new String[]{
       "core.asl",
       "vision.asl",
-      "nao/naodemo.asl",
-      "dialogue/nlg.asl",
-      "dialogue/handleSemantics.asl",
-      "dialogue/nlu.asl",
     };
 
     String gmArgs = String.format("-beliefinitfile demos.pl agents/boxbot.pl " +
